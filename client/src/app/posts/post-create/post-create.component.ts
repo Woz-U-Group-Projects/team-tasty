fbceb346fb9b4ba41de022047fedd840584a530a
ashleyth <ashley.th@hotmail.com>
2019-04-28 23:26:00 -0500

    Added form error handling and post model file for post-create and post-list components

--- client/src/app/posts/post-create/post-create.component.ts
+++ client/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -11,10 +13,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter();
-
-  onAddPost() {
-    const post = {title: this.enteredTitle, content: this.enteredContent};
+  @Output() postCreated = new EventEmitter<Post>();
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
     this.postCreated.emit(post);
   }
 }
