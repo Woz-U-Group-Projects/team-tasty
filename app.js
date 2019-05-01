48bc70a177de3754028ce6b1b894d26ab09d9c9e
ashleyth <ashley.th@hotmail.com>
2019-04-30 19:58:50 -0500

    Does push work?

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect("mongodb://user:password1@ds121730.mlab.com:21730/tasty-database");
 
 app.use(logger('dev'));
 app.use(express.json());
