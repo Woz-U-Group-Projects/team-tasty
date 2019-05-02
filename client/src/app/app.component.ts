81aa9131c58496c3b2e921aa1d8c530f1c4856c3
Raymond Gregory <vinedsel@gmail.com>
2019-05-01 19:25:08 -0500

    Added packages and numerous changes to these files.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,56 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
-}
-
-
-// slick
-
-@Component({
-  ...
-})
-class ExampleComponent {
+//   title = 'client';
+// }
+//
+//
+// // slick
+//
+// @Component({
+//
+// })
+// class ExampleComponent {
 
   slides = [
-    {img: "team-tasty/images/angelchickenpasta.jpg"},
-    {img: "team-tasty/images/bananabread.jpg"},
-    {img: "team-tasty/images/bananacreampie.jpg"},
-    {img: "team-tasty/images/chocolatemoussepie.jpg"},
-    {img: "team-tasty/images/creamofmushroomsoup.jpg"},
-    {img: "team-tasty/images/easycreamcheesedanish.jpg"},
-    {img: "team-tasty/images/easymeatloaf.jpg"},
-    {img: "team-tasty/images/famousporkchops.jpg"},
-    {img: "team-tasty/images/fluffypancakes.jpg"},
-    {img: "team-tasty/images/friedfish.jpg"},
-    {img: "team-tasty/images/lasagnaalfredorollups.jpg"},
-    {img: "team-tasty/images/rigatoniallagenovese.jpg"},
-    {img: "team-tasty/images/sausagepotatokalesoup.jpg"},
-    {img: "team-tasty/images/shrimpscampipasta.jpg"},
-    {img: "team-tasty/images/spanishgarlicshrimp.jpg"},
-    {img: "team-tasty/images/spinachandbaconquiche.jpg"},
-    {img: "team-tasty/images/springtimespaghetti.jpg"}
+    {img: "assets/img/angelchickenpasta.jpg"},
+    {img: "assets/img/bananabread.jpg"},
+    {img: "assets/img/bananacreampie.jpg"},
+    {img: "assets/img/chocolatemoussepie.jpg"},
+    {img: "assets/img/creamofmushroomsoup.jpg"},
+    {img: "assets/img/easycreamcheesedanish.jpg"},
+    {img: "assets/img/easymeatloaf.jpg"},
+    {img: "assets/img/famousporkchops.jpg"},
+    {img: "assets/img/fluffypancakes.jpg"},
+    {img: "assets/img/friedfish.jpg"},
+    {img: "assets/img/lasagnaalfredorollups.jpg"},
+    {img: "assets/img/rigatoniallagenovese.jpg"},
+    {img: "assets/img/sausagepotatokalesoup.jpg"},
+    {img: "assets/img/shrimpscampipasta.jpg"},
+    {img: "assets/img/spanishgarlicshrimp.jpg"},
+    {img: "assets/img/spinachandbaconquiche.jpg"},
+    {img: "assets/img/springtimespaghetti.jpg"}
   ];
-  slideConfig = {"slidesToShow": 17, "slidesToScroll": 17};
+
+  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1, "autoplay": true};
 
   addSlide() {
-    this.slides.push({img: "team-tasty/images/angelchickenpasta.jpg"}),
-    this.slides.push({img: "team-tasty/images/bananabread.jpg"}),
-    this.slides.push({img: "team-tasty/images/bananacreampie.jpg"}),
-    this.slides.push({img: "team-tasty/images/chocolatemoussepie.jpg"}),
-    this.slides.push({img: "team-tasty/images/creamofmushroomsoup.jpg"}),
-    this.slides.push({img: "team-tasty/images/easycreamcheesedanish.jpg"}),
-    this.slides.push({img: "team-tasty/images/easymeatloaf.jpg"}),
-    this.slides.push({img: "team-tasty/images/famousporkchops.jpg"}),
-    this.slides.push({img: "team-tasty/images/fluffypancakes.jpg"}),
-    this.slides.push({img: "team-tasty/images/friedfish.jpg"}),
-    this.slides.push({img: "team-tasty/images/lasagnaalfredorollups.jpg"}),
-    this.slides.push({img: "team-tasty/images/rigatoniallagenovese.jpg"}),
-    this.slides.push({img: "team-tasty/images/sausagepotatokalesoup.jpg"}),
-    this.slides.push({img: "team-tasty/images/shrimpscampipasta.jpg"}),
-    this.slides.push({img: "team-tasty/images/spanishgarlicshrimp.jpg"}),
-    this.slides.push({img: "team-tasty/images/spinachandbaconquiche.jpg"}),
-    this.slides.push({img: "team-tasty/images/springtimespaghetti.jpg"})
+    this.slides.push({img: "assets/img/angelchickenpasta.jpg"}),
+    this.slides.push({img: "assets/img/bananabread.jpg"}),
+    this.slides.push({img: "assets/img/bananacreampie.jpg"}),
+    this.slides.push({img: "assets/img/chocolatemoussepie.jpg"}),
+    this.slides.push({img: "assets/img/creamofmushroomsoup.jpg"}),
+    this.slides.push({img: "assets/img/easycreamcheesedanish.jpg"}),
+    this.slides.push({img: "assets/img/easymeatloaf.jpg"}),
+    this.slides.push({img: "assets/img/famousporkchops.jpg"}),
+    this.slides.push({img: "assets/img/fluffypancakes.jpg"}),
+    this.slides.push({img: "assets/img/friedfish.jpg"}),
+    this.slides.push({img: "assets/img/lasagnaalfredorollups.jpg"}),
+    this.slides.push({img: "assets/img/rigatoniallagenovese.jpg"}),
+    this.slides.push({img: "assets/img/sausagepotatokalesoup.jpg"}),
+    this.slides.push({img: "assets/img/shrimpscampipasta.jpg"}),
+    this.slides.push({img: "assets/img/spanishgarlicshrimp.jpg"}),
+    this.slides.push({img: "assets/img/spinachandbaconquiche.jpg"}),
+    this.slides.push({img: "assets/img/springtimespaghetti.jpg"})
   }
 
   removeSlide() {
