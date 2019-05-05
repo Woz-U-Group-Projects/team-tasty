5473c856e4cccc4b201d121d03aba8e8c3e71f5c
Raymond Gregory <vinedsel@gmail.com>
2019-05-04 19:28:22 -0500

    Installed boot strap and created navbar component.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppMainComponent } from './app-main/app-main.component'
+import { Navbar } from './navbar/navbar.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: '', pathMatch:"full", redirectTo:"/carousel"},
+  //{path: "carousel", component: CarouselComponent},
+  {path:"home", component: AppMainComponent}, {path:"menu", component: Navbar}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
