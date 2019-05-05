6f5a3eddb19ace68fbcd28c9fc15e1f60ce1a188
Raymond Gregory <vinedsel@gmail.com>
2019-05-04 20:06:59 -0500

    Revert "Installed boot strap and created navbar component."
    This reverts commit 5473c856e4cccc4b201d121d03aba8e8c3e71f5c.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppMainComponent } from './app-main/app-main.component'
-import { Navbar } from './navbar/navbar.component'
 
-const routes: Routes = [
-  //{path: '', pathMatch:"full", redirectTo:"/carousel"},
-  //{path: "carousel", component: CarouselComponent},
-  {path:"home", component: AppMainComponent}, {path:"menu", component: Navbar}
-];
-
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
