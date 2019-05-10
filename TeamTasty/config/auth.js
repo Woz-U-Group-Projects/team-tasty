976b7f43d4bc08646a31e4e42e57e20af93c8ddc
Donn Gerald <godsmathetes@gmail.com>
2019-05-09 20:18:40 -0400

    Added JWT Tokens and Oauth

---
+++ TeamTasty/config/auth.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const models = require('../models/index');
+
+module.exports = {
+    signUser: function(user) {
+      const token = jwt.sign(
+        {
+          Username: user.Username,
+          UserId: user.UserId
+        },
+        'secret',
+        {
+          expiresIn: '1h'
+        }
+      );
+      return token;
+    },
+  
+    verifyUser: function(req, res, next) {
+      try {
+        let token = req.cookies.jwt;
+        const decoded = jwt.verify(token, 'secret');
+        req.userData = decoded;
+        models.users
+          .findOne({
+            where: {
+              UserId: decoded.UserId
+            }
+          })
+          .then(user => {
+            console.log(user.UserId);
+            req.user = user;
+            next();
+          });
+      } catch (err) {
+        console.log(err);
+        return res.status(401).json({
+          message: 'Auth Failed'
+        });
+      }
+    },
+  
+    hashPassword: function(plainTextPassword) {
+      let salt = bcrypt.genSaltSync(10);
+      let hash = bcrypt.hashSync(plainTextPassword, salt);
+      return hash;
+    }
+  };
