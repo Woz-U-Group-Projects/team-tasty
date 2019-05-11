843ea0a1f62cc2aaca0d76af4cc2ec81dac7a0c8
Raymond Gregory <vinedsel@gmail.com>
2019-05-11 18:08:43 -0500

    Changed all quotations to stabs.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppMainComponent } from "./app-main/app-main.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { FavoritesComponent } from "./favorites/favorites.component";
-import { AppComponent } from "./app.component";
-import { MenuModule } from "./menu/menu.module";
+import { AppMainComponent } from './app-main/app-main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { AppComponent } from './app.component';
+import { MenuModule } from './menu/menu.module';
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "/home" },
-  { path: "home", component: AppMainComponent },
-  { path: "menu", component: NavbarComponent },
-  { path: "favorites", component: FavoritesComponent },
-  { path: "login", component: AppComponent }
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: AppMainComponent },
+  { path: 'menu', component: NavbarComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'login', component: AppComponent }
 ];
 
 @NgModule({
