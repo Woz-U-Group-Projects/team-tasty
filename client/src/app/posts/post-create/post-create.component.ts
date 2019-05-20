8b54cb4cf3321babaf397bb24b44fbc43cb3a447
Ashley <ashley.th@hotmail.com>
2019-05-20 10:09:28 -0500

    forgot posts skeleton

---
+++ client/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+
+export class PostCreateComponent {
+
+}
